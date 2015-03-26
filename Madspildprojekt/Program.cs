8143d5ee07600ea0044ea6bcdbd14b6c73d6f18a
RasmusKrusaa <rasmuskrusaa@gmail.com>
2015-03-26 14:08:17 +0100

    Revert "Revert "Addet alle ting""
    This reverts commit bebe3551cca0c1e448c337a4779af80a32c88cc0.

---
+++ Madspildprojekt/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Madspildprojekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hej svanser!");
+            Console.WriteLine("Ja hej Rasmus");
+            Console.WriteLine("Hej Mark");
+            Console.WriteLine("Hej Mark");
+            Console.WriteLine("Hej Mark");
+            Console.WriteLine("hejdu");
+            Console.ReadKey();
+        }
+    }
+}
