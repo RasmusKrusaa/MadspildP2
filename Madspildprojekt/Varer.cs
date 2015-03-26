7a770dee6bbb7aa978939b3ebf95083e3d713427
RasmusKrusaa <rasmuskrusaa@gmail.com>
2015-03-26 14:40:23 +0100

    Varer.cs
    Varer classen

---
+++ Madspildprojekt/Varer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Madspildprojekt
+{
+    public class Varer
+    {
+        string _navn;
+        int _stk;
+        double _vaegt;
+
+        public Varer(string navn, int stk, double vaegt)
+        {
+            _navn = navn;
+            _stk = stk;
+            _vaegt = vaegt;
+        }
+    }
+}
