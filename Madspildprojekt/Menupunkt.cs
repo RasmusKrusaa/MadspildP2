6a5d8e8aed117c664ce3980e7c60134f6f6b4c35
RasmusKrusaa <rasmuskrusaa@gmail.com>
2015-04-09 09:42:57 +0200

    Menupunkt class

---
+++ Madspildprojekt/Menupunkt.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Madspildprojekt
+{
+    class Menupunkt : Menu
+    {
+        public Menupunkt(string title) : base(title)
+        {
+        }
+
+        public override void select()
+        {
+            Console.Clear();
+            Console.WriteLine("Ged");
+            Console.CursorVisible = false;
+            ConsoleKeyInfo KeyPressed = Console.ReadKey();
+            if (KeyPressed.Key == ConsoleKey.Escape)
+            {
+                Environment.Exit(0);
+            }
+            else if (KeyPressed.Key == ConsoleKey.Backspace)
+            {
+                displayMenu();
+            }
+        }
+    }
+}
