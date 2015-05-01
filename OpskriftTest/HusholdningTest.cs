eb50763205ed8fe0afbe29fc00957d5e4b7a12c5
BIlgram <Rasmusbilgram94@hotmail.com>
2015-05-01 12:18:53 +0200

    Mere Test

---
+++ OpskriftTest/HusholdningTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Madspildprojekt;
+
+namespace MadspildprojektTests
+{
+    [TestFixture]
+    class HusholdningTest
+    {
+        public void DatoAdvarsel()
+        {
+            //Arrange
+            DateTime Idag = DateTime.Today;
+            //Act
+            //Assert
+
+        }
+    }
+}
